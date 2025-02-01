c7c91505fbeef1bf849184b183a51fe62a1f8dc0
uudx <155887302+uudx@users.noreply.github.com>
2025-02-01 19:26:59 +0500

    Create 3task.py

---
+++ classes/3task.py
@@ -0,0 +1,14 @@
+class Shape:
+    def area(self):
+        return 0
+class Square(Shape):
+    def __init__(self, length):
+        self.length = length
+    def area(self):
+        return self.length * self.length
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    def area(self):
+        return self.length * self.width
