8b1f4454af362f010ded1e045abd69cb1cd86569
uudx <155887302+uudx@users.noreply.github.com>
2025-02-03 17:53:02 +0500

    Update 6task.py

--- classes/6task.py
+++ classes/6task.py
@@ -1,3 +1,3 @@
-x = lambda x, count = 0: [False if (x % i) == 0 else True for i in range(2,x)]
+x = lambda x:[False if (x % i) == 0 else True for i in range(2,x)]
 number = int(input())
 print("it is a prime number" if False not in x(number) else "it is not a prime number") if number != 0 or 1 else print("it is not a prime number")
