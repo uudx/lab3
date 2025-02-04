dadc88836af811d86759e2550918d4ca3487bc57
uudx <155887302+uudx@users.noreply.github.com>
2025-02-05 01:24:15 +0500

    Create 14task.py

---
+++ function1/14task.py
@@ -0,0 +1,7 @@
+from 4task import is_prime
+from 7task import has_33
+from 8task import spy_game
+
+print(is_prime("1 6 8 55 4 3556 2 7 3 11 57 9"))
+print(has_33([1,2,3,4,5,6,4,3,3,3,2,1]))
+print(spy_game([6,4,3,7,9,6,3,0,9,5,0,6,4,2,7]))
