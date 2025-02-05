02330987f18b79d77c4ec96b051f940e59f1b7cb
uudx <155887302+uudx@users.noreply.github.com>
2025-02-05 11:34:00 +0500

    Create 1task.py

---
+++ function2/1task.py
@@ -0,0 +1,96 @@
+movies = [
+{
+"name": "Usual Suspects", 
+"imdb": 7.0,
+"category": "Thriller"
+},
+{
+"name": "Hitman",
+"imdb": 6.3,
+"category": "Action"
+},
+{
+"name": "Dark Knight",
+"imdb": 9.0,
+"category": "Adventure"
+},
+{
+"name": "The Help",
+"imdb": 8.0,
+"category": "Drama"
+},
+{
+"name": "The Choice",
+"imdb": 6.2,
+"category": "Romance"
+},
+{
+"name": "Colonia",
+"imdb": 7.4,
+"category": "Romance"
+},
+{
+"name": "Love",
+"imdb": 6.0,
+"category": "Romance"
+},
+{
+"name": "Bride Wars",
+"imdb": 5.4,
+"category": "Romance"
+},
+{
+"name": "AlphaJet",
+"imdb": 3.2,
+"category": "War"
+},
+{
+"name": "Ringing Crime",
+"imdb": 4.0,
+"category": "Crime"
+},
+{
+"name": "Joking muck",
+"imdb": 7.2,
+"category": "Comedy"
+},
+{
+"name": "What is the name",
+"imdb": 9.2,
+"category": "Suspense"
+},
+{
+"name": "Detective",
+"imdb": 7.0,
+"category": "Suspense"
+},
+{
+"name": "Exam",
+"imdb": 4.2,
+"category": "Thriller"
+},
+{
+"name": "We Two",
+"imdb": 7.2,
+"category": "Romance"
+}
+]
+
+def rating(name):
+    for i in range(0, len(movies)):
+        if movies[i]["name"].lower().replace(" ", "") == name.lower().replace(" ", ""):
+            if movies[i]["imdb"] > 5.5:
+                return True
+            else:
+                return False
+            break
+        elif i == len(movies)-1:
+            if movies[i]["name"].lower().replace(" ", "") == name.lower().replace(" ", ""):
+                if movies[i]["imdb"] > 5.5:
+                    return True
+                else:
+                    return False
+                break
+            else:
+                return "The film was not founded"
+print(rating(input()))
